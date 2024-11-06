97d4950628e22a312a3b29ecc87f0ef8da77917e
ASdddd <sdfajejewkkk@mail.com>
2024-11-06 14:11:57 +0200

    initial configuration add selenium dependency add webdrivers for popular web browsers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WebDriver driver = new EdgeDriver();
+
+        System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("http://www.wikipedia.org");
+        driver.close();
     }
 }
