f265195bebc95b1c57d8a3b0235d4d7e6bca502c
Boanta Cristi <cristi.boanta0@gmail.com>
2024-11-22 15:14:23 +0200

    remove unnecessary code for a bug that was fixed on W3School

--- src/main/java/org/example/web/w3Schools/W3SchoolsElementHandler.java
+++ src/main/java/org/example/web/w3Schools/W3SchoolsElementHandler.java
@@ -1,7 +1,6 @@
 package org.example.web.w3Schools;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,9 +23,10 @@ public class W3SchoolsElementHandler {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         //we need to wait until the YouTube playlist loads because it automatically scrolls to it
+        /* Was fixed on website, and it is no longer necessary
         WebElement playlist = driver.findElement(By.cssSelector("#yt-playlist-container"));
         wait.until(ExpectedConditions.visibilityOf(playlist));
-        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0)");
+        ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 0)");*/
 
         WebElement iFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#main > div:nth-child(7) > iframe")));
         driver.switchTo().frame(iFrame);
