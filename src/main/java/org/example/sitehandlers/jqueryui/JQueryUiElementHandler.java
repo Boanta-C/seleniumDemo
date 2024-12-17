dfe6c6069699d5a9a088819cf4d4148020049256
Boanta Cristi <cristi.boanta0@gmail.com>
2024-12-17 12:12:07 +0200

    update Edge driver and Selenium for current browser version add two methods to perform actions

--- src/main/java/org/example/sitehandlers/jqueryui/JQueryUiElementHandler.java
+++ src/main/java/org/example/sitehandlers/jqueryui/JQueryUiElementHandler.java
@@ -3,12 +3,23 @@ package org.example.sitehandlers.jqueryui;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class JQueryUiElementHandler {
 
     public static void performDragAndDrop(WebDriver driver) {
+        driver.switchTo().frame(driver.findElement(By.cssSelector("#content > iframe")));
+
         WebElement draggable = driver.findElement(By.id("draggable"));
         WebElement droppable = driver.findElement(By.id("droppable"));
+        Actions dragAndDrop = new Actions(driver);
+
+        dragAndDrop.dragAndDrop(draggable, droppable).build().perform();
+    }
+
+    public static void performRightClick(WebDriver driver) {
+        Actions rightClick = new Actions(driver);
+        rightClick.contextClick().build().perform();
     }
 
 }
--- src/main/java/org/example/tests/JQueryUiTest.java
+++ src/main/java/org/example/tests/JQueryUiTest.java
@@ -1,5 +1,6 @@
 package org.example.tests;
 
+import org.example.sitehandlers.jqueryui.JQueryUiElementHandler;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -14,7 +15,8 @@ public class JQueryUiTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get("https://jqueryui.com/droppable/");
-
+        JQueryUiElementHandler.performDragAndDrop(driver);
+        JQueryUiElementHandler.performRightClick(driver);
 
 
         driver.quit();
